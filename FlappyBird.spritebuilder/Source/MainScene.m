e0cc08ed03787f5554ab6993852254b8423772dd
Bhawan Virk <bhawanvirk@Bhawans-MacBook-Pro.local>
2015-02-27 17:30:03 -0800

    Automatically Committed for MakeGamesWithUs

--- FlappyBird.spritebuilder/Source/MainScene.m
+++ FlappyBird.spritebuilder/Source/MainScene.m
@@ -142,7 +142,7 @@
         // if the left corner is one complete width off the screen, move it to the right
         if (groundScreenPosition.x <= (-1 * ground.contentSize.width)) {
             NSLog(@"called inside if statement");
-            ground.position = ccp(ground.position.x + 2 * ground.contentSize.width, ground.position.y);
+            ground.position = ccp(ground.position.x + 2 * ground.contentSize.width, ground.position.y + 1);
         }
     }
     
